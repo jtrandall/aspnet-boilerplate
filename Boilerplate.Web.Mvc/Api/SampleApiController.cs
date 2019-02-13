f4bc5b6d3d0163f501deaa99a4031bb0724975cd
Jeremy Randall <jrandall@gfsd.net>
2019-02-13 10:41:24 -0500

    Added Camel Case API Controller attribute

---
+++ Boilerplate.Web.Mvc/Api/SampleApiController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Boilerplate.Web.Mvc.Models;
+
+namespace Boilerplate.Web.Mvc.Api
+{
+    [Route("api/sample")]
+    [CamelCaseControllerConfig]
+    public class SampleApiController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            var results = new List<Person>
+            {
+                new Person { Name = "Henry Smith", Address="403 Main Street" },
+                new Person { Name = "Julie Vasquez", Address="931 Starks Street" },
+                new Person { Name = "Holly Ford", Address="192 Hillside Boulevard" }
+            };
+
+            return Ok(results);
+        }
+    }
+}
---
+++ Boilerplate.Web.Mvc/Models/CamelCaseControllerConfigAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using Newtonsoft.Json.Serialization;
+
+namespace Boilerplate.Web.Mvc.Models
+{
+    /// <summary>
+    /// Forces an ApiController class that is marked with this attribute to return results in camel-cased JSON format.
+    /// </summary>
+    /// <remarks>
+    /// Rather than make a global change that causes all controllers to return camel-cased json, this can be used selectively
+    /// to ensure existing calls will still work as expected. (Client behavior will not change on classes not marked
+    /// with this attribute)
+    /// </remarks>
+    public sealed class CamelCaseControllerConfigAttribute : Attribute, IControllerConfiguration
+    {
+        public void Initialize(HttpControllerSettings controllerSettings, HttpControllerDescriptor controllerDescriptor)
+        {
+            var formatter = controllerSettings.Formatters.OfType<JsonMediaTypeFormatter>().Single();
+            controllerSettings.Formatters.Remove(formatter);
+
+            formatter = new JsonMediaTypeFormatter
+            {
+                SerializerSettings = { ContractResolver = new CamelCasePropertyNamesContractResolver() },
+                SupportedMediaTypes = { new System.Net.Http.Headers.MediaTypeHeaderValue("text/html") }
+            };
+
+            controllerSettings.Formatters.Add(formatter);
+        }
+    }
+}
